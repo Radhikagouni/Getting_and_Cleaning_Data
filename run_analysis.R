7bad553813d712b93d407ef49531bd8cbfc4a34c
Radhikagouni <radhika.g530@gmail.com>
2015-04-26 13:59:26 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -12,8 +12,7 @@
 
 ##########################################################################################################
 
-# Clean up workspace
-rm(list=ls())
+# Clean up workspace rm(list=ls())
 
 # 1. Merge the training and the test sets to create one data set.
 
