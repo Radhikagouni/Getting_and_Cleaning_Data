4d0c0f3548d662a0f32e3bd508030367dfdf4ab7
Radhikagouni <radhika.g530@gmail.com>
2015-04-26 13:51:50 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -10,7 +10,11 @@
 # 4. Appropriately label the data set with descriptive activity names. 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-###########################################################################################################
+##########################################################################################################
+
+
+# Clean up workspace
+# rm(list=ls())
 
 # 1. Merge the training and the test sets to create one data set.
 
